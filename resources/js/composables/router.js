82a00c531696280d228156cb00c5aa9a24a6ff52
Loic <loic63130@gmail.com>
2026-05-29 14:29:15 +0200

    Ajout des routes hash vue

---
+++ resources/js/composables/router.js
@@ -0,0 +1,35 @@
+import { computed, onBeforeUnmount, onMounted, shallowRef } from 'vue';
+
+export function useHashRoute(routes) {
+  const defaultRoute = routes[0];
+  const currentRoute = shallowRef(defaultRoute);
+
+  function syncRouteFromUrl() {
+    const hash = window.location.hash;
+    currentRoute.value = routes.find(route => route.hash === hash) ?? defaultRoute;
+  }
+
+  function navigateTo(hash) {
+    window.history.pushState(null, '', hash);
+    syncRouteFromUrl();
+  }
+
+  onMounted(() => {
+    if (window.location.hash === '') {
+      window.history.replaceState(null, '', defaultRoute.hash);
+    }
+    syncRouteFromUrl();
+    window.addEventListener('popstate', syncRouteFromUrl);
+    window.addEventListener('hashchange', syncRouteFromUrl);
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener('popstate', syncRouteFromUrl);
+    window.removeEventListener('hashchange', syncRouteFromUrl);
+  });
+
+  const currentComponent = computed(() => currentRoute.value.component);
+  syncRouteFromUrl();
+
+  return { currentComponent, currentRoute, navigateTo };
+}
--- resources/js/public/App.vue
+++ resources/js/public/App.vue
@@ -1,7 +1,27 @@
 <script setup>
 import PublicDefaultLayout from './layouts/PublicDefaultLayout.vue'
+import { useHashRoute } from '@/composables/router'
+
+import Home from './views/Home.vue'
+import Trophees from './views/Trophees.vue'
+import Label from './views/Label.vue'
+import Information from './views/Information.vue'
+import Contact from './views/Contact.vue'
+
+const routes = [
+  { hash: '#/home',         key: 'home',         component: Home },
+  { hash: '#/trophee',      key: 'trophee',      component: Trophees },
+  { hash: '#/label',        key: 'label',        component: Label },
+  { hash: '#/informations', key: 'informations', component: Information },
+  { hash: '#/contact',      key: 'contact',      component: Contact },
+  { hash: '#/prendre-rdv',  key: 'rdv',          component: Home },
+]
+
+const { currentComponent, currentRoute, navigateTo } = useHashRoute(routes)
 </script>
 
 <template>
-  <PublicDefaultLayout />
+  <PublicDefaultLayout :current="currentRoute.key">
+    <component :is="currentComponent" />
+  </PublicDefaultLayout>
 </template>
